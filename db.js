3b9a4fed7a74f3f4ac088a498b4aacbd92645ea1
najaram <arambulo.jan@gmail.com>
2019-07-30 16:45:09 +0800

    update crappy code

--- db.js
+++ db.js
@@ -1,8 +1,9 @@
+require('dotenv').config()
 const mysql = require('mysql');
 const connection = mysql.createConnection({
-    host: 'localhost',
-    user: 'root',
-    password: 'secret',
+    host: process.env.DB_HOST,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASS,
     database: 'job-scrapper'
 });
 
